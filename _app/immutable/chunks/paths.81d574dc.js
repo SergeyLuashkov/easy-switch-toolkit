972290c1ecc76941a5a16325e78da1aaa3a355e4
SergeyLuashkov <sergeyluashkov@yandex.com>
2023-08-10 21:15:19 +0300

    gh-pages

---
+++ _app/immutable/chunks/paths.81d574dc.js
@@ -0,0 +1,1 @@
+var s;const a=((s=globalThis.__sveltekit_1a4eue7)==null?void 0:s.base)??"/easy-switch-toolkit";var e;const t=((e=globalThis.__sveltekit_1a4eue7)==null?void 0:e.assets)??a;export{t as a,a as b};
